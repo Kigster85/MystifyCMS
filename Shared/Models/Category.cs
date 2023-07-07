0ef9d0436bd929ba4ae8ce9a5784de1504dc549b
Kigster85 <philbillson85@gmail.com>
2023-07-06 22:46:57 -0300

    minor changes

--- Shared/Models/Category.cs
+++ Shared/Models/Category.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Models
 {
     internal class Category
     {
+        [Key]
+
+        public int CatergoyId { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+
+        public string ThumbnailImagePath { get; set; }
+
+        [Required]
+        [MaxLength(128]
+
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1024)]
+        public string Description { get; set; }
+
+
     }
 }
