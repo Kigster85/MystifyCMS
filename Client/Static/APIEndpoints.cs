ff892da310caf50d878d2d326fa5ee6552af98f5
Kigster85 <philbillson85@gmail.com>
2023-07-11 00:50:26 -0300

    Setting up endpoints for API calls and making sure one get request at a time happens.

---
+++ Client/Static/APIEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace Client.Static
+{
+    internal static class APIEndpoints
+    {
+#if DEBUG
+        //DO THING//
+        internal const string ServerBaseUrl = "https://localhost:5003";
+#else
+        //DO PROD THING//
+        internal const string ServerBaseUrl = "https://appname.azurewebsites.net";
+#endif
+
+        internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+    }
+}
