58958e2d13948f0fbb832db94783a69496878d2c
Kigster85 <philbillson85@gmail.com>
2023-07-12 20:35:14 -0300

    changing swagger from dev only to published

--- Server/Program.cs
+++ Server/Program.cs
@@ -30,12 +30,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
 app.UseCors("CorsPolicy");
 
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
