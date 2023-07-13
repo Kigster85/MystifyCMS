b5843ddd8b67b43a44b0f8ef4f4e4abfff28065a
Kigster85 <philbillson85@gmail.com>
2023-07-12 22:16:55 -0300

    Applying New API Endpoints to client side files

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -7,7 +7,7 @@
         internal const string ServerBaseUrl = "https://localhost:5003";
 #else
         //DO PROD THING//
-        internal const string ServerBaseUrl = "https://appname.azurewebsites.net";
+        internal const string ServerBaseUrl = "https://mystifycms-server.azurewebsites.net";
 #endif
 
         internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
