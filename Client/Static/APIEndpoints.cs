bbb9a5dcb35909d65fe86442aaaaded559765126
Kigster85 <philbillson85@gmail.com>
2023-08-15 19:59:01 -0300

    checked logic and functionality inc debugging to make sure all is working.

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -7,11 +7,10 @@
         internal const string ServerBaseUrl = "https://localhost:5003";
 #else
         //DO PROD THING//
-        internal const string ServerBaseUrl = "https://mystifyserver.azurewebsites.net";
+        internal const string ServerBaseUrl = "https://server.outofdabox.ca";
 #endif
 
-        internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+		internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
 		internal readonly static string s_imageUpload = $"{ServerBaseUrl}/api/imageupload";
-
 	}
 }
