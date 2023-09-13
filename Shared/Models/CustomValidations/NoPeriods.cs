d35b1ab6161296a4ce0bf23347ff919749f27d24
Kigster85 <philbillson85@gmail.com>
2023-09-13 16:50:38 -0300

    Custom Validation added to the category and post forms in admin section.

---
+++ Shared/Models/CustomValidations/NoPeriods.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models.CustomValidations
+{
+	public sealed class NoPeriods : ValidationAttribute
+	{
+		public override bool IsValid(object value)
+		{
+			string input = value.ToString();
+
+			bool noPeriods = input.Contains('.') == false;
+
+			return noPeriods;
+
+		}
+	}
+}
