b2d141333969ff7256fd0a59d369e0b436efef30
Kiggi <75673610+Kigster85@users.noreply.github.com>
2023-09-21 23:45:20 -0300

    Setting up Client security

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,8 @@
+using Blazored.LocalStorage;
 using Client;
+using Client.Providers;
 using Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,5 +13,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton<HttpClient>();
 builder.Services.AddSingleton<InMemoryDatabaseCache>();
 
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AppAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+    provider.GetRequiredService<AppAuthenticationStateProvider>());
 
 await builder.Build().RunAsync();
